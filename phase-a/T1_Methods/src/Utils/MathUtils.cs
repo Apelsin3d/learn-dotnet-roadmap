8b1ad835eb984d4672dedde846f7169537bdafcf
Евгений Смиленко <smilenko.e@gmail.com>
2025-09-02 11:48:31 +0300

    T1 homework

---
+++ phase-a/T1_Methods/src/Utils/MathUtils.cs
@@ -0,0 +1,69 @@
+﻿namespace Utils
+{
+    public class MathUtils
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static bool TryDivide(int a, int b, out int result)
+        {
+            if (b == 0)
+            {
+                result = 0;
+                return  false;
+            }
+            result = a / b;
+            return true;
+        }
+
+        public static int Increment(ref int a)
+        {
+            return a++;
+        }
+
+        public static int Decrement(ref int a)
+        {
+            return a--;
+        }
+
+        public static int Square(in int a)
+        {
+            return a * a;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Swap(ref int a, int b)
+        {
+            return a = b;
+        }
+
+        public static int ArraySum(in int[] a)
+        {
+            return a.Sum();
+        }
+
+        public static bool ArrayMinMaxValue(int[] a, out int min, out int max)
+        {
+            if (a.Length == 0)
+            {
+                min = 0;
+                max = 0;
+                return false;
+            }
+
+            min = a.Min();
+            max = a.Max();
+            return true;
+        }
+    }
+}
---
+++ phase-a/T1_Methods/tests/demoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Utils;
+
+namespace demoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, DemoApp!");
+
+            var a = new int[] { 1,5,8 };
+
+            if (MathUtils.ArrayMinMaxValue(a, out var min, out var max))
+            {
+                Console.WriteLine($"Array max value:{max}, min value:{min}");
+            }
+            else
+            {
+                Console.WriteLine("Array is empty");
+            }
+        }
+    }
+}
