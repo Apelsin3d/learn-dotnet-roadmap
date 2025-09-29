eccd9dc998e0ea00c31971fc16456f1e5a197fe5
Evheniy Smilenko <smilenko.e@gmail.com>
2025-09-29 21:29:14 +0300

    refactoring Car class and add constructor

--- phase-a/T2_OOP/OOP/Car.cs
+++ phase-a/T2_OOP/OOP/Car.cs
@@ -6,6 +6,12 @@ namespace OOP
         public string Brand { get; set; }
         public int Year { get; set; }
 
+        public Car(string brand, int year)
+        {
+            Brand = brand;
+            Year = year;
+        }
+
         public void ShowInfo()
         {
             Console.WriteLine($"Car is {Brand}, {Year} year of manufacture");
--- phase-a/T2_OOP/demoApp/Program.cs
+++ phase-a/T2_OOP/demoApp/Program.cs
@@ -6,15 +6,10 @@ namespace demoApp
     {
         static void Main(string[] args)
         {
-            var car1 = new Car();
-            car1.Brand = "Toyota";
-            car1.Year = 2025;
+            var car1 = new Car("Toyota",2025);
             car1.ShowInfo();
 
-
-            var car2 = new Car();
-            car2.Brand = "BMW";
-            car2.Year = 2023;
+            var car2 = new Car("BMW", 2023);
             car2.ShowInfo();
         }
     }
